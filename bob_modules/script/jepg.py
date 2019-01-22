ea0cde3f82f978abb1fad8c0bfd5e41eb5afe8a1
bobtthp <gzqtianc@gmail.com>
2019-01-22 13:34:56 +0800

    Update jepg.py

--- bob_modules/script/jepg.py
+++ bob_modules/script/jepg.py
@@ -32,19 +32,12 @@ header_base = {
     "Cache-Control": "max-age=0",
     "Connection": "keep-alive",
     "Content-Type": "application/x-www-form-urlencoded",
-    # "Cookie": "gr_user_id=7982af3d-e5e5-4f71-9eb1-01b58a36a2bc; _ga=GA1.2.909036554.1517660261; Hm_lvt_53c8bf761df44282a0cf7d4949581592=1517660262; JSESSIONID=3C1C34F6619E176BE2B8F2A863DB51E1",
-    # "Host": "kq2.qk365.com",
-    # "Origin": "http://kq2.qk365.com",
     "Referer": "http://kq2.qk365.com/login",
-    # "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
 }
 
 # if __name__ == '__main__':
-#    #with open(img, 'wb') as im:
-#        im.write(valcode.content)
-#    getcode(img)
-#    requests.post()
+
 data['username'] = 'xxx'
 data['password'] = 'xxx'
 data['validateCode'] = 'nrts'
